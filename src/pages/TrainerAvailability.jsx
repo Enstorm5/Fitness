2d1dc576973cf2f92353b0d7d35e31ff317f9a23
chevenx <chevenx.24@gmail.com>
2024-05-18 12:00:00 +0530

    "TrainerAvailabilityPage"

---
+++ src/pages/TrainerAvailability.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TrainerAvailability = () => {
+  return (
+    <div>
+      <h1>Trainer Availability</h1>
+      <p>Here you can see the availability of our trainers.</p>
+    </div>
+  );
+};
+
+export default TrainerAvailability;
